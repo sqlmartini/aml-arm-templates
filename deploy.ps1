3dc873b12a30576d2ebb9020290f25fb5488ade9
Anthony Martin <anthma@microsoft.com>
2020-06-12 16:09:07 -0400

    initial commit

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+﻿#Login to Azure
+Connect-AzAccount
+
+#Create new resource group for deployment
+New-AzResourceGroup -Name sqlmartini -Location "eastus2"
+
+#Deploy ARM Template without VNET
+#New-AzResourceGroupDeployment -ResourceGroupName sqlmartini -TemplateFile template.json -TemplateParameterFile parameters.json
+
+#Deploy ARM Template with VNET
+New-AzResourceGroupDeployment -ResourceGroupName sqlmartini -TemplateFile template_VNET.json -TemplateParameterFile parameters.json
